031b56a930246df2e1a99d913b8e063259aa5c05
Miguel Viegas <miegas4@users.noreply.github.com>
2017-11-26 20:11:48 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,45 +1,43 @@
-drop table teste cascade;
+DROP TABLE IF EXISTS teste CASCADE;
+DROP TABLE IF EXISTS categoria CASCADE;
+DROP TABLE IF EXISTS categoria_simples CASCADE;
+DROP TABLE IF EXISTS super_categoria CASCADE;
+DROP TABLE IF EXISTS constituida CASCADE;
+DROP TABLE IF EXISTS fornecedor CASCADE;
+DROP TABLE IF EXISTS produto CASCADE;
+DROP TABLE IF EXISTS fornece_sec CASCADE;
+DROP TABLE IF EXISTS corredor CASCADE;
+DROP TABLE IF EXISTS prateleira CASCADE;
+DROP TABLE IF EXISTS planograma CASCADE;
+DROP TABLE IF EXISTS evento_reposicao CASCADE;
+DROP TABLE IF EXISTS reposicao CASCADE;
+
+
 
 CREATE TABLE teste (
-	primario integer,
-	secundario integer NOT NULL,
+	primario INT,
+	secundario INT NOT NULL,
 	
 	PRIMARY KEY(primario)
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Coisas a rever / melhorar (isto ja diz respeito ao projeto, mas ta super incompleto)
-
 CREATE TABLE categoria (
 	nome VARCHAR(40),
 	
-	PRIMARY KEY(nome),
+	PRIMARY KEY(nome)
 );
 
 CREATE TABLE categoria_simples (
-	nome VARCHAR(40),
+	nome VARCHAR(40) REFERENCES categoria(nome),
 	
-	PRIMARY KEY(nome) REFERENCES categoria(nome),
+	PRIMARY KEY(nome)
 );
 
 CREATE TABLE super_categoria (
 	nome VARCHAR(40),
 	
 	PRIMARY KEY(nome),
-	FOREIGN KEY(nome) REFERENCES categoria(nome),
+	FOREIGN KEY(nome) REFERENCES categoria(nome)
 );
 
 CREATE TABLE constituida (
@@ -51,28 +49,68 @@ CREATE TABLE constituida (
 	FOREIGN KEY(categoria) REFERENCES categoria(nome)
 );
 
+CREATE TABLE fornecedor (
+	nif INT,
+	nome VARCHAR(255),
+
+	PRIMARY KEY (nif)
+);
+
 CREATE TABLE produto (
-	ean BIGINT(13),
+	ean BIGINT,
 	design TEXT,
 	categoria VARCHAR(40),
-	forn_primario INT(9), /* TO BE COMPLETED
-	data ,
+	forn_primario INT,
+	data DATE,
 
 	PRIMARY KEY(ean),
 	FOREIGN KEY(categoria) REFERENCES categoria(nome),
-	FOREIGN KEY(forn_primario) REFERENCES fornecedor
+	FOREIGN KEY(forn_primario) REFERENCES fornecedor(nif)
+
+	/*CHECK ean BETWEEN 0 AND 1000000000*/
 );
 
-CREATE TABLE fornecedor (
-	nif ,
-	nome VARCHAR(255)
-); */
+CREATE TABLE fornece_sec(
+	nif INTEGER,
+	ean BIGINT,
+
+	PRIMARY KEY(nif, ean),
+	FOREIGN KEY(nif) REFERENCES fornecedor(nif),
+	FOREIGN KEY(ean) REFERENCES produto(ean)
+);
+
+CREATE TABLE corredor (
+	nro SMALLSERIAL,
+	largura NUMERIC(3,2),
+
+	PRIMARY KEY(nro)
+);
+
+CREATE TABLE prateleira (
+	nro SMALLSERIAL,
+	lado CHAR(3),
+	altura NUMERIC(2,2),
+
+	PRIMARY KEY(nro, lado, altura),
+	FOREIGN KEY(nro) REFERENCES corredor(nro)
+);
+
+
+
+
+
+
+
+
+
+
+
 
 CREATE TABLE planograma (
-	ean INT,
+	ean BIGINT,
     nro SMALLSERIAL,
     lado CHAR(3),
-    altura INT,
+    altura NUMERIC(2,2),
     face SMALLINT,
     unidades SMALLINT,
     loc INT,
@@ -86,18 +124,18 @@ CREATE TABLE planograma (
 
 CREATE TABLE evento_reposicao (
 	operador INT,
-    instante TIME,
+    instante TIMESTAMP,
     
     PRIMARY KEY(operador, instante)
 );
 
 CREATE TABLE reposicao (
-    ean TEXT,
+    ean BIGINT,
     nro SMALLSERIAL,
     lado CHAR(3),
-    altura INT,
+    altura NUMERIC(2,2),
     operador INT,
-    instante TIME,
+    instante TIMESTAMP,
     unidades INT,
     
     PRIMARY KEY(ean, nro, lado, altura, operador, instante),
@@ -106,7 +144,7 @@ CREATE TABLE reposicao (
     FOREIGN KEY(lado) REFERENCES planograma(lado),
     FOREIGN KEY(altura) REFERENCES planograma(altura),
     FOREIGN KEY(operador) REFERENCES evento_reposicao(operador),
-    FOREIGN KEY(operador) REFERENCES evento_reposicao(instante),
+    FOREIGN KEY(operador) REFERENCES evento_reposicao(instante)
     
-    CHECK (lado = 'dir' OR lado = 'esq')
+    /* CHECK (lado = 'dir' OR lado = 'esq') */
 );
