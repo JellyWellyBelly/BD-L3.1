ea69bebed0ca0f95d445a8908a70ff279297861b
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2017-11-27 17:21:29 +0000

    Delete populate.sql

--- populate.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO teste(primario, secundario) VALUES (1, 1);
-INSERT INTO teste(primario, secundario) VALUES (2, 1);
-INSERT INTO teste(primario, secundario) VALUES (3, 2);
-INSERT INTO teste(primario, secundario) VALUES (4, 2);
-INSERT INTO teste(primario, secundario) VALUES (5, 3);
-INSERT INTO teste(primario, secundario) VALUES (6, 3);
